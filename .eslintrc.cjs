153c3fc77f05bc6dc338dad08a208777be211076
JoseGuzman <chato4010@gmail.com>
2022-10-07 00:47:45 -0500

    Normalize all the line endings

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,12 +4,10 @@ module.exports = {
     es2021: true,
   },
   extends: 'airbnb-base',
-  overrides: [
-  ],
+  overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  rules: {
-  },
+  rules: {},
 };
--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,23 +1,26 @@
+/* eslint-disable import/prefer-default-export */
 import JWT from 'jsonwebtoken';
 import dotenv from 'dotenv';
+
 dotenv.config();
 
 export const verifyToken = (req, res, next) => {
-        const token = req.headers['x-access-token'];
-        if (!token) return res.status(401).json({ message: 'No token provided' });
-
-    try {
-        const decode = JWT.verify(token, process.env.SECRET_KEY)
-
-        req.user = decode;
-   
-        next();
-            
-
-    } catch (error) {
-        res.status(401).json({ message: 'Unauthorized' });
+    const token = req.headers['x-access-token'];
+    if (!token) {
+        return res.status(401).json({
+            message: 'No token provided',
+        });
     }
 
+    try {
+        const decode = JWT.verify(token, process.env.SECRET_KEY);
 
+        req.user = decode;
 
-}
+        next();
+    } catch (error) {
+        res.status(401).json({
+            message: 'Unauthorized',
+        });
+    }
+};
